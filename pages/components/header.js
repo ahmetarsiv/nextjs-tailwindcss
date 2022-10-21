885912a88580ae3ec83514db3f84eb27fdf179ef
Ahmet Sefa Arşiv <ahmetarsivi@gmail.com>
2022-10-21 14:23:44 +0300

    update header

--- pages/components/header.js
+++ pages/components/header.js
@@ -9,13 +9,15 @@ const Header = ({ className, children }) => (
         <div className="container mx-auto sm:hidden md:block lg:block xl:block 2xl:block hidden py-2.5">
             <div className="flex justify-between">
                 <span className="font-medium text-yellow-400 text-2xl">{children}</span>
-                <div className="flex text-white bg-purple-700 hover:bg-purple-900 rounded-lg">
-                    <UserIcon className="w-12 h-12 mr-2"/>
-                    <div className="inline-flex flex-col w-full">
-                        <span className="font-bold">Example Name</span>
-                        <a href="#" className="text-yellow-400 hover:text-yellow-500 focus:text-yellow-600 font-bold">Account</a>
+                <a href="#">
+                    <div className="flex text-white bg-purple-700 hover:bg-purple-900 rounded-lg">
+                        <UserIcon className="w-12 h-12 mr-2"/>
+                        <div className="inline-flex flex-col w-full">
+                            <span className="font-bold">Example Name</span>
+                            <span className="text-yellow-400 font-bold">Account</span>
+                        </div>
                     </div>
-                </div>
+                </a>
             </div>
             <ul className="text-white font-bold text-base list-none flex">
                 <li><a href="#" className="pr-12 hover:text-yellow-400">
